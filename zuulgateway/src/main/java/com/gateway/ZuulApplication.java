e7b71dc3c38821c1e2d6b3d3236893d1fea49298
chengz <15955702565@163.com>
2020-04-28 19:42:28 +0800

    bugfix

--- zuulgateway/src/main/java/com/gateway/ZuulApplication.java
+++ zuulgateway/src/main/java/com/gateway/ZuulApplication.java
@@ -2,10 +2,12 @@ package com.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 public class ZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class,args);
