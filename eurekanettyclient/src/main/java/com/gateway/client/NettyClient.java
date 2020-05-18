373c7a6084088753040dda06507e772919451e06
chengz <15955702565@163.com>
2020-05-18 22:42:42 +0800

    bugfix

--- eurekanettyclient/src/main/java/com/gateway/client/NettyClient.java
+++ eurekanettyclient/src/main/java/com/gateway/client/NettyClient.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.regex.Pattern;
 
 public class NettyClient {
--- eurekanettyserver/src/main/java/com/gateway/server/ServerIniterHandler.java
+++ eurekanettyserver/src/main/java/com/gateway/server/ServerIniterHandler.java
@@ -6,7 +6,6 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
-
 public class ServerIniterHandler extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
