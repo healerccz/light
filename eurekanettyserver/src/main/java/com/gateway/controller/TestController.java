8a39da9ffd9700638b5991425633abadf62e4070
chengz <15955702565@163.com>
2020-05-21 08:31:43 +0800

    bugfix

--- eurekanettyserver/src/main/java/com/gateway/controller/TestController.java
+++ eurekanettyserver/src/main/java/com/gateway/controller/TestController.java
@@ -2,15 +2,12 @@ package com.gateway.controller;
 
 import com.gateway.redis.BaseRedisService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("t")
 public class TestController {
-
     @Autowired
     private BaseRedisService baseRedisService;
 
