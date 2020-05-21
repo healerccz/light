7b98ae999e699c1726d445046cf63584cdfed2e9
chengz <15955702565@163.com>
2020-05-21 10:12:11 +0800

    bugfix

--- eurekanettyserver/src/main/java/com/gateway/redis/BaseRedisService.java
+++ eurekanettyserver/src/main/java/com/gateway/redis/BaseRedisService.java
@@ -3,10 +3,11 @@ package com.gateway.redis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
-@Component
+@Service
 public class BaseRedisService {
 
     @Autowired
