e6797c8deebde7163f8d7f3968f0cf4084e6d70e
chengz <15955702565@163.com>
2020-05-21 08:23:19 +0800

    bugfix

--- eurekanettyclient/src/main/java/com/gateway/controller/NettyClientHandler.java
+++ eurekanettyclient/src/main/java/com/gateway/controller/NettyClientHandler.java
@@ -1,6 +1,7 @@
 package com.gateway.controller;
 
 import com.gateway.client.NettyClient;
+import com.gateway.redis.BaseRedisService;
 import com.gateway.response.ClientReturnData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,9 @@ public class NettyClientHandler {
     @Autowired
     private NettyClient nettyClient;
 
+    @Autowired
+    private BaseRedisService baseRedisService;
+
     @GetMapping("command")
     public ClientReturnData sendCom(@RequestParam String cmd) {
         ClientReturnData returnData = new ClientReturnData();
@@ -18,7 +22,8 @@ public class NettyClientHandler {
         flag = nettyClient.sendCom(cmd);
         returnData.code = flag ? 200+"" : 500+"";
         returnData.msg = flag ? "操作成功" : "操作失败";
-
+        String value = baseRedisService.get("key");
+        System.out.println("这是redis中key对应对的值："+value);
         return returnData;
     }
 }
--- eurekanettyserver/src/main/java/com/gateway/controller/TestController.java
+++ eurekanettyserver/src/main/java/com/gateway/controller/TestController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Configuration
-@RequestMapping("light")
+@RequestMapping("t")
 public class TestController {
 
     @Autowired
